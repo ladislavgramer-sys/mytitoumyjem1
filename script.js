a9e1d4a8292dee81f9a1cf3bd5ea16143f4ada62
ladislavgramer-sys <ladislavgramer@gmail.com>
2025-09-21 19:23:17 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,9 +1,1 @@
-
-document.querySelectorAll('a[href^="#"]').forEach(a=>{
-  a.addEventListener('click',e=>{
-    e.preventDefault();
-    const id=a.getAttribute('href').substring(1);
-    const el=document.getElementById(id);
-    if(el){el.scrollIntoView({behavior:'smooth',block:'start'})}
-  });
-});
+console.log('My Ti To Umyjem - web je připraven.');
